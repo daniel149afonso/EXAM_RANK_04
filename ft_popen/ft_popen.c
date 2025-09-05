1666ee6d2632a19f1f55d21695fe6221506d6a96
daniel <daniel-congo2@hotmail.com>
2025-09-05 19:48:52 +0200

    correction des return des bon pipe

--- ft_popen/ft_popen.c
+++ ft_popen/ft_popen.c
@@ -6,9 +6,15 @@
 /*   By: daniel149afonso <daniel149afonso@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/11 23:35:18 by daniel149af       #+#    #+#             */
-/*   Updated: 2025/09/05 19:40:28 by daniel149af      ###   ########.fr       */
+/*   Updated: 2025/09/05 19:48:47 by daniel149af      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+// Mode 'r' → read : le parent veut lire la sortie du fils
+// → dup2(fd[1], STDOUT_FILENO) dans l’enfant → parent retourne fd[0].
+
+// Mode 'w' → write : le parent veut écrire dans l’entrée du fils
+// → dup2(fd[0], STDIN_FILENO) dans l’enfant → parent retourne fd[1].
 
 #include <stdlib.h> //exit()
 #include <sys/wait.h> //pid_t
@@ -34,14 +40,14 @@ int	ft_popen(const char *file, char *const argv[], char type)
 		if (type == 'r')
 		{
 			close(pipefd[0]);
-			if (dup2(pipefd[1], STDIN_FILENO) < 0)
-				exit(-1);
+			if (dup2(pipefd[1], STDIN_FILENO) < 0) // le parent veut lire la sortie du fils
+				exit(-1); //ferme aussi automatiquement les fds
 		}
 		else
 		{
 			close(pipefd[1]);
-			if (dup2(pipefd[0], STDOUT_FILENO) < 0)
-				exit(-1);
+			if (dup2(pipefd[0], STDOUT_FILENO) < 0) //le parent veut écrire dans l'entrée du fils.
+				exit(-1); //ferme aussi automatiquement les fds
 		}
 		close(pipefd[0]);
 		close(pipefd[1]);
@@ -51,11 +57,11 @@ int	ft_popen(const char *file, char *const argv[], char type)
 	if (type == 'r')
 	{
 		close(pipefd[1]);
-		return (pipefd[0]);
+		return (pipefd[0]); // permet de lire la sortie du fils
 	}
 	else
 	{
 		close(pipefd[0]);
-		return (pipefd[1]);
+		return (pipefd[1]); // permet d'écrire dans l'entrée du fils
 	}
 }
